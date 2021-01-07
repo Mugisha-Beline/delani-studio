51a58c6536a55387b609f02f6df26b54f1a4fde5
Mugisha-Beline <belinemugisha21@gmail.com>
2021-01-07 10:07:43 +0200

    added js for the first time

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    //Portfolio label on hover effect
+    $('#portfolio span').addClass("hide");
+    $('#portfolio .doings').hover(function() {
+      /*fade in code*/
+      $(this).find('span').removeClass('hide');
+      $(this).find('.doings').addClass('imageEffect');
+    }, function() {
+      /*fade out code*/
+      $(this).find('span').addClass('hide');
+      $(this).find('.doings').removeClass('imageEffect');
+    });
